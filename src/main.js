98d71605cdac76d2ff6cd6ad554ba0d10a2da081
Nick Topacio <ntopacio25@gmail.com>
2023-06-14 01:15:07 -0400

    Add list add functionality, no check-off

--- src/main.js
+++ src/main.js
@@ -1,4 +1,44 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
-import "../styles/components/todo-box.css";
+import "../styles/components/todo.css";
 import "../styles/utils.css";
+
+let form = document.getElementById("todo__form");
+let ul = document.getElementById("todo__items");
+let listHeight = 20;
+let listChange = 3.5;
+
+// changes the list height depending on if an item is added or deleted
+function changeListHeight() {
+  let todo_box = document.getElementById("todo__box");
+  listHeight += listChange;
+  todo_box.style.height = listHeight + "rem";
+}
+
+// creates the actual list item in ul tag
+function createListElement(input) {
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  changeListHeight();
+}
+
+// adds the list if input is non-empty
+function addListAfterClick(input) {
+  if (input.value.length > 0) {
+    createListElement(input);
+  }
+}
+
+// listen for check-offs
+for (let i = 0; i < ul.childNodes.length; i++) {}
+
+// listens for the actual 'add' button press
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // prevents form from auto-submitting
+
+  let input = document.getElementById("todo__task");
+  addListAfterClick(input);
+
+  input.value = ""; // clears task name after submitting
+});
